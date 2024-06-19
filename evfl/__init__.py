e996fb18b389d584811f7653ca0646074b404d59
Léo Lam <leo@leolam.fr>
2024-06-19 21:33:33 +0100

    Update versioneer for Python 3.12 compat

--- evfl/__init__.py
+++ evfl/__init__.py
@@ -5,6 +5,5 @@ from evfl.event import Event, ActionEvent, SwitchEvent, ForkEvent, JoinEvent, Su
 from evfl.evfl import EventFlow
 from evfl.flowchart import Flowchart
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
